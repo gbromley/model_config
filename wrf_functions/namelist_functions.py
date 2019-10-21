b1a34b103a983adafe714b9fc3f463a14db6e280
gbromley <gabriel.bromley@montana.edu>
2019-10-21 10:59:58 -0600

    Scripting real and wrf together

--- wrf_functions/namelist_functions.py
+++ wrf_functions/namelist_functions.py
@@ -7,9 +7,10 @@
 
 import f90nml
 import datetime as dt
+import os
 
 
-def advance_restart(prev_namelist, interval):
+def advance_restart(prev_namelist, interval, output_dir):
     ###
     #Advances the namelist by specified interval in weeks
     #Takes a namelist, finds the start variables adds
@@ -20,7 +21,10 @@ def advance_restart(prev_namelist, interval):
         f = open(prev_namelist, 'r')
     except IOError:
         print("Could not read file:", prev_namelist)
-
+    else:
+        f.close()
+    if (os.path.exists(output_dir)==False):
+        return 1
 
     with open(prev_namelist, 'r') as old_nl:
         namelist = f90nml.read(old_nl)
@@ -28,8 +32,22 @@ def advance_restart(prev_namelist, interval):
         start_month = namelist['time_control']['start_month']
         start_day = namelist['time_control']['start_day']
 
+        end_year = namelist['time_control']['end_year']
+        end_month = namelist['time_control']['end_month']
+        end_day = namelist['time_control']['end_year']
+
+
+
         current_restart = dt.date(start_year,start_month, start_day)
         new_restart = current_restart + dt.timedelta(weeks=interval)
+
+        if (new_restart.year >= end_year and new_restart.month >= end_month and new_restart.day >= end_day):
+            print("Simulation has ended")
+            return 2
+
+
+
+
         namelist.end_comma=True
         namelist['time_control']['start_year'] = new_restart.year
         namelist['time_control']['start_month'] = new_restart.month
@@ -37,7 +55,8 @@ def advance_restart(prev_namelist, interval):
 
 
         try:
-            namelist.write(new_restart.isoformat()+'_namelist.input')
+            namelist.write(output_dir+new_restart.isoformat()+'_namelist.input')
+            return 0
         except IOError:
             print('File already exists',new_restart.isoformat()+'_namelist.input')
 
--- wrf_functions/next_restart.py
+++ wrf_functions/next_restart.py
@@ -3,3 +3,31 @@
 # Author: Gabe Bromley
 ###
 
+from wrf_functions import namelist_functions as nf
+import sys
+import os
+
+
+def main():
+
+
+
+    namelist_file = sys.argv[0]
+    interval = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    ### Try and open the namelist to makesure it exists
+    try:
+        f = open(namelist_file, 'r')
+    except OSError:
+        print('cannot open', namelist_file)
+    else:
+        f.close()
+    ### Run the advance restart code
+    return_code = nf.advance_restart(namelist_file, 8 ,output_dir)
+    ### Pass return code from advance_restart to bash
+    return return_code
+
+
+if __name__ == "__main__":
+    main()
