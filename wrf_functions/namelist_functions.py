4a063897ece64a740ad3a90fab45911a6853ee80
gbromley <gabriel.bromley@montana.edu>
2020-06-08 10:37:34 -0600

    Bug squashing in model restart code.

--- wrf_functions/namelist_functions.py
+++ wrf_functions/namelist_functions.py
@@ -22,29 +22,46 @@ def next_restart(prev_namelist_path):
         f.close()
 
 
-    ### Get the last restart file by creation time ###
-    list_of_restarts = glob.glob(prev_namelist_path +'/wrfrst*')  # * means all if need specific format then *.csv
+    ### Get the latest restart file by creation time ###
+    list_of_restarts = glob.glob(prev_namelist_path +'/wrfrst*')  #gets list of all restart files
     latest_file = max(list_of_restarts, key=os.path.getctime)
     print(latest_file)
     restart = latest_file.split('/')[-1]
     ### format of restart file name: wrfrst_d01_2010-11-22_00:00:00 ###
+    ### Get the new restart date ###
     rst_year = int(restart[11:15])
     rst_month =  int(restart[16:18])
     rst_day = int(restart[19:21])
-
+    ### Set the number of days the model should run, make sure its n+1 of the restart interval ###
+    adv_days = 17
     current_restart = dt.date(rst_year, rst_month, rst_day)
     with open(prev_namelist, 'r') as old_nl:
         os.rename(prev_namelist, prev_namelist_path + '/backup.namelist.input')
         namelist = f90nml.read(old_nl)
+        ### Get the start time ###
         start_year = namelist['time_control']['start_year']
         start_month = namelist['time_control']['start_month']
         start_day = namelist['time_control']['start_day']
+        ### Get the end time ###
+        end_year = namelist['time_control']['end_year']
+        end_month = namelist['time_control']['end_month']
+        end_day = namelist['time_control']['end_day']
 
+        ### Advance the end date by adv_days number of days ###
+        current_end_date = dt.date(end_year, end_month, end_day)
+        new_restart_end_date = current_end_date + dt.timedelta(days = adv_days)
+        print('End date will be advanced by '+adv_days+' days')
 
+        ### Write out the new start and end days ###
         namelist.end_comma = True
         namelist['time_control']['start_year'] = current_restart.year
         namelist['time_control']['start_month'] = current_restart.month
         namelist['time_control']['start_day'] = current_restart.day
+
+        namelist['time_control']['end_year'] = new_restart_end_date.year
+        namelist['time_control']['end_month'] = new_restart_end_date.month
+        namelist['time_control']['end_day'] = new_restart_end_date.day
+
 
         try:
             namelist.write(prev_namelist_path+'/namelist.input')
